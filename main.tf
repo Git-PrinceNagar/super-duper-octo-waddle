81b64d2381f31ef4bbaa5d97bf738b0ae70692b8
prince nagar <princenagar@princes-MacBook-Air.local>
2025-09-01 08:58:39 +0530

    1 rd added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "thela"
   location = "central india"
   }
+
+  resource "azurerm_resource_group" "rg2" {
+  name     = "thela2"
+  location = "central india"
+  }
