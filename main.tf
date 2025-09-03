2b22093951c33925ef969fd37f482e51a49ff6b5
prince nagar <prince.nagar22@gmail..com>
2025-09-03 12:36:41 +0530

    rg name changes

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "thela"
+  name     = "thelanew"
   location = "central india"
   }
 
   resource "azurerm_resource_group" "rg2" {
-  name     = "thela2"
+  name     = "thelanew2"
   location = "central india"
   }
 
 resource "azurerm_resource_group" "rg3" {
-  name     = "thela3"
+  name     = "thelanew3"
   location = "central india"
 }
 
